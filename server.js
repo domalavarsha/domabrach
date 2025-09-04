513782214d1a0a5e8a497087fcac6e74cfd6d23c
Domala Varsha <2205A41130@sru.edu.in>
2025-09-04 16:40:54 +0530

    second

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-consloe.log("helo")
+// Import express
+const express = require("express");
+
+// Create an express app
+const app = express();
+
+// Define port
+const port = 3000;
+
+// Create a basic route
+app.get("/", (req, res) => {
+  res.send("SucessFully insta;lled the node  🚀");
+});
+
+app.get("/home", (req, res) => {
+  res.send("dont install this 🚀");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`);
+});
